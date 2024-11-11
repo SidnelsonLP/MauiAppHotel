e11af7eccfe588dbc695d36b1e212ed225377063
Sidnelson Porto <sidnelsonlp@gmail.com>
2024-11-11 10:44:02 -0300

    Adicionar recursos ao app.

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,8 +1,41 @@
 ﻿
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto =110.00,
+                ValorDiariaCriancas = 55.0
+
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 88.00,
+                ValorDiariaCriancas = 40.0
+
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCriancas = 25.00
+
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCriancas = 17.5
+
+            }
+        };
         public App()
         {
             InitializeComponent();
